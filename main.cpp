eef89a79886b2f24e77b7ebf89456c94854d61da
dommiloo <156959975+dommiloo@users.noreply.github.com>
2024-01-17 13:59:51 -0800

    add dominic bates

--- main.cpp
+++ main.cpp
@@ -5,5 +5,6 @@ int main()
     std::cout << "Hello World" << std::endl;
     std::cout << "Blake Mosley" << std::endl;
     std::cout << "Ryan Reeves" << std::endl;
+     std::cout << "Dominic Bates" << std::endl;
 
 }
